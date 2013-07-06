69f0e4fa179c224d94f0d6307ad60d7fb6c25101
Lukas Braun <koomi@hackerspace-bamberg.de>
2013-07-06 22:08:51 +0200

    Added !inspace

--- Commands.hs
+++ Commands.hs
@@ -2,19 +2,41 @@
 
 module Commands (commands) where
 
+import Control.Monad
+import Data.Monoid ((<>))
 import Data.Maybe
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.ByteString (ByteString)
+import Data.ByteString.Char8 (pack)
+import Data.ByteString (ByteString, cons)
+import qualified Data.ByteString.Lazy.Char8 as LBS
 import Network.SimpleIRC
+import Network.HTTP
+import Data.Aeson
+import Data.Aeson.Types (parseMaybe)
 
 import Utils
 
 type CommandMap = Map ByteString ([ByteString] -> EventFunc)
 
 commands :: CommandMap
-commands = M.fromList [("echo", echo)
+commands = M.fromList [ ("echo", echo)
+                      , ("inspace", inspace)
                       ]
 
 echo :: [ByteString] -> EventFunc
-echo args s msg = sendMsg s (fromJust $ mOrigin msg) $ unwordsBS args
+echo args s msg = respond s msg $ unwordsBS args
+
+
+inspace :: [ByteString] -> EventFunc
+inspace args s msg = do
+    resp <- simpleHTTP (getRequest url) >>= getResponseBody
+    let obj    = decode $ LBS.pack resp
+        nicks  = maybe "Error fetching nicknames" unwordsBS $
+          obj >>= parseMaybe (mapM (.: "nickname") <=< (.: "members_present"))
+        opener = maybe "Members present: "
+                       (\x -> pack (show (x :: Int )) <> " members present: ")
+                       (obj >>= parseMaybe (.: "members"))
+    respondNick s msg $ opener <> nicks
+  where
+    url = "http://status.bckspc.de/status.php?response=json"
--- Utils.hs
+++ Utils.hs
@@ -1,12 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Utils
   ( space
   , wordsBS
   , unwordsBS
+  , respond
+  , respondNick
   ) where
 
 import Data.Word
 import Data.ByteString (ByteString)
+import Data.Monoid ((<>))
+import Data.Maybe  (fromJust)
 import qualified Data.ByteString as BS
+import Network.SimpleIRC
 
 space :: Word8
 space = 32
@@ -16,3 +23,16 @@ wordsBS = BS.split space
 
 unwordsBS :: [ByteString] -> ByteString
 unwordsBS = BS.intercalate (BS.singleton space)
+
+respond :: MIrc -> IrcMessage -> ByteString -> IO ()
+respond s initial = sendMsg s (fromJust $ mOrigin initial)
+
+respondNick :: MIrc -> IrcMessage -> ByteString -> IO ()
+respondNick s init resp =
+    respond s init $
+      if nick == origin
+        then resp
+        else nick <> ": " <> resp
+  where
+    nick   = fromJust $ mNick init
+    origin = fromJust $ mOrigin init
