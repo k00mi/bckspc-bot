8d0f3ea3ceb876cc0a5bd00af0085fa39b1e367a
Lukas Braun <koomi@hackerspace-bamberg.de>
2013-07-07 02:10:55 +0200

    Added !pizza

--- Commands.hs
+++ Commands.hs
@@ -6,12 +6,17 @@ module Commands
   ) where
 
 import           Control.Monad
+import           Control.Applicative
 import           Data.Monoid                ((<>))
 import           Data.Maybe
-import           Data.ByteString.Char8      (pack)
+import           Text.Read                  (readMaybe)
+import           Data.Char                  (isDigit)
 import           Data.ByteString            (ByteString, cons)
+import           Data.ByteString.Char8      (pack, unpack)
+import qualified Data.ByteString.Char8      as BSC
 import qualified Data.ByteString.Lazy.Char8 as LBS
 import qualified Data.Map                   as M
+import           Control.Concurrent         (forkIO, threadDelay)
 import           Network.SimpleIRC
 import           Network.HTTP
 import           Data.Aeson
@@ -25,6 +30,7 @@ type CommandMap = M.Map ByteString ([ByteString] -> EventFunc)
 commands :: CommandMap
 commands = M.fromList [ ("echo", echo)
                       , ("inspace", inspace)
+                      , ("pizza", pizza)
                       ]
 
 
@@ -44,3 +50,34 @@ inspace args s msg = do
     respondNick s msg $ opener <> nicks
   where
     url = "http://status.bckspc.de/status.php?response=json"
+
+
+pizza :: [ByteString] -> EventFunc
+pizza args s msg =
+    if null args
+      then notifyIn $ mins 15
+      else maybe error notifyIn . getTime $ head args
+  where
+    notifyIn t = do
+        forkIO $ do
+            threadDelay t
+            say "Time is up!"
+        say "I won't forget it!"
+
+    error = say "Could not parse duration"
+
+    getTime str = do
+        (numStr, suffix) <- BSC.unsnoc str
+        if isDigit suffix
+          then fmap mins . readMaybe $ unpack str
+          else case suffix of
+                's' -> secs  <$> readMaybe (unpack numStr);
+                'm' -> mins  <$> readMaybe (unpack numStr);
+                'h' -> hours <$> readMaybe (unpack numStr);
+                 _  -> Nothing
+
+    secs  x = x * 10^6
+    mins  x = x * secs 60
+    hours x = x * mins 60
+
+    say = respondNick s msg
